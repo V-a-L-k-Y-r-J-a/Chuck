6af81918785d389cca486efe419b9725eb53ce90
Vlada <104246034+V-a-L-k-Y-r-J-a@users.noreply.github.com>
2022-12-01 13:51:08 +0700

    Initial commit

---
+++ Chuck/ApiProvider.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuck
+{
+    public class ApiProvider
+    {
+        HttpClient httpClient;
+        public ApiProvider()
+        {
+            httpClient = new HttpClient();
+        }
+        public async Task<Rootobject> GetJokesAsyncs(string query)
+        {
+            string request = $"https://api.chucknorris.io/jokes/search?query={query}";
+            HttpResponseMessage response =
+                (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            JObject jObject = JObject.Parse(responseBody);
+            Rootobject rootobject = jObject.ToObject<Rootobject>();
+            return rootobject;
+        }        
+        
+        public async Task<string[]> GetCategoriesAsyncs()
+        {
+            string request = $"https://api.chucknorris.io/jokes/categories";
+            HttpResponseMessage response =
+                (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            JArray jar = JArray.Parse(responseBody);
+            return jar.ToObject<string[]>();
+        }
+
+        public async Task<Rootobject> GetRanAsyncs()
+        {
+            string request = $"https://api.chucknorris.io/jokes/random";
+            HttpResponseMessage response =
+                (await httpClient.GetAsync(request)).EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            JObject jObject = JObject.Parse(responseBody);
+            Rootobject? rootobject = jObject.ToObject<Rootobject>();
+            return rootobject;
+        }
+
+    }
+}
---
+++ Chuck/Searchs.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chuck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Searchs : Window
+    {
+        ApiProvider Api { get; }
+        List<Result> Results { get; set; }
+        List<ComboBoxItem> categories { get; set; }
+        public Searchs()
+        {
+            InitializeComponent();  
+            Results = new List<Result>();
+            Api = new ApiProvider();
+            LoadCategories();
+            LoadRanDGridAsync();
+        }
+        async void LoadCategories() 
+        {
+            var c = await Api.GetCategoriesAsyncs();
+            categories= new List<ComboBoxItem>();
+            foreach (var item in c)
+            categories.Add(new ComboBoxItem() { Content = item });
+            CatCBox.DataContext = categories;
+        }
+        async void LoadJokesDGridAsync() 
+        {
+            Rootobject? rootobject = await Api.GetJokesAsyncs(SearchJokeTBox.Text);
+            //foreach (var item in rootobject.result)
+            //    Results.Add(item);
+            JokesDGrid.ItemsSource = rootobject.result;
+        }
+
+
+        async void LoadRanDGridAsync()
+        {
+            Rootobject? rootobject = await Api.GetRanAsyncs();
+
+            JokesDGrid.ItemsSource = rootobject.result; 
+        }
+
+
+          
+
+
+
+
+        private void SearchJokeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoadJokesDGridAsync();
+        }
+
+        private void LogOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void CatCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void RandomJokeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoadRanDGridAsync();
+        }
+
+     
+    }
+}
